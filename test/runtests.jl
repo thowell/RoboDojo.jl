8ad8289caa44338d980cf222f5efde5b3ee55e70
simon-lc <simonlc@stanford.edu>
2022-05-08 16:46:48 -0700

    adding infeasible controls to quadruped4

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,20 +8,20 @@ using FiniteDiff
 using RoboDojo
 
 # Utils
-# include("utils.jl")
-#
-# # Solver
-# include("solver/lu.jl")
-# include("solver/random_qp.jl")
-# include("solver/soc.jl")
-#
-# # Robots
-# include("robots/hopper.jl")
-# include("robots/biped.jl")
-# include("robots/quadruped.jl")
-#
-# # Simulator
-# include("simulator/utils.jl")
+include("utils.jl")
+
+# Solver
+include("solver/lu.jl")
+include("solver/random_qp.jl")
+include("solver/soc.jl")
+
+# Robots
+include("robots/hopper.jl")
+include("robots/biped.jl")
+include("robots/quadruped.jl")
+
+# Simulator
+include("simulator/utils.jl")
 include("simulator/simulate.jl")
 include("simulator/codegen.jl")
 include("simulator/dynamics.jl")
